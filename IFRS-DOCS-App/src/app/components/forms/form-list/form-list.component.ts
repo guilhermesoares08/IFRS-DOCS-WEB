1989480bb11dc6d7bee8bda03d2c1f44996c5b09
Guilherme Soares <guilhermesoares1996@hotmail.com>
2022-09-08 00:45:07 -0300

    adjust pagination to search by page and term

--- IFRS-DOCS-App/src/app/components/forms/form-list/form-list.component.ts
+++ IFRS-DOCS-App/src/app/components/forms/form-list/form-list.component.ts
@@ -6,6 +6,9 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from "ngx-spinner";
 import { Router } from '@angular/router';
+import { PaginatedResult, Pagination } from 'src/app/models/Pagination';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-form-list',
@@ -16,91 +19,118 @@ import { Router } from '@angular/router';
 export class FormListComponent implements OnInit {
   modalRef?: BsModalRef;
   message?: string;
-
+  
   public forms: Form[] = [];
-  public filteredForms: Form[] = [];
-  private _filterList: string = '';
   public Filtrov: number = 2;
-
+  public pagination = {} as Pagination;
+  termoBuscaChanged: Subject<string> = new Subject<string>();
+  
   constructor(private formService: FormService,
     private modalService: BsModalService,
-    private toastrService: ToastrService,
+    private toastr: ToastrService,
     private spinner: NgxSpinnerService,
     private router: Router) { }
-
-  public ngOnInit(): void {
-    this.spinner.show();
-    this.getForms();
-  }
-  
-  public getForms(): void{
-    this.formService.getFormByUser(1).subscribe({
-      next:(response: Form[]) => {
-        this.forms = response;
-        this.filteredForms = this.forms;
-      },
-      error: (error: any) =>{
-        this.spinner.hide();
-        this.toastrService.error('Erro ao carregar os formulários', 'Erro');
-      },
-      complete: () => this.spinner.hide() 
-    });
-  }
-
-  public getStatusColor(status: any){
-    let cl ='btn';
     
-    switch(status.toUpperCase()){
-      case 'CANCELADA':
-        cl += ' btn-danger';
-        break;
-      case 'ATENDIDA':        
-        cl += ' btn-success';
-        break;
-      case 'EM ANDAMENTO':
-         cl += ' btn-secondary';
-         break;
-      case 'AGUARDANDO RETIRADA':
-         cl += ' btn-info';
-         break;
-      case 'PENDENTE':
-        cl += ' btn-primary';
-        break;
-    }
-    return cl += ' btn-sm';
-  }
+    public ngOnInit(): void {
+      this.pagination = {
+        currentPage: 1,
+        itemsPerPage: 3,
+        totalItems: 1
+      } as Pagination; 
 
-  public get filterList() {
-    return this._filterList;
-  }
+        this.getForms();
+      }      
 
-  public set filterList(value: string) {
-    this._filterList = value;
-    this.filteredForms = this.filterList ? this.filterForms(this.filterList) : this.forms;
-  }
-
-  public filterForms(filterBy: string): Form[] {
-    filterBy = filterBy.toLocaleLowerCase();
-    return this.forms.filter(
-      (dtoResponse: any) => dtoResponse.name.toLocaleLowerCase().indexOf(filterBy) !== -1 
-    );
-  }
-
-  openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
-  }
- 
-  confirm(): void {
-    this.modalRef?.hide();
-    this.toastrService.success("mensagem corpo", "cabeçalho");
-  }
- 
-  decline(): void {
-    this.modalRef?.hide();
-  }
-
-  formDetail(formId: number): void{
-    this.router.navigate([`forms/detail/${formId}`])
-  }
-
-}
+      public getForms(): void {
+        this.spinner.show();
+    
+        this.formService
+          .getForms(this.pagination.currentPage, this.pagination.itemsPerPage)
+          .subscribe(
+            (paginatedResult: PaginatedResult<Form[]>) => {
+              this.forms = paginatedResult.result;
+              this.pagination = paginatedResult.pagination;
+            },
+            (error: any) => {
+              this.spinner.hide();
+              this.toastr.error('Erro ao Carregar os Forms', 'Erro!');
+            }
+          )
+          .add(() => this.spinner.hide());
+      }
+      
+      public getStatusColor(status: any){
+        let cl ='btn';
+        
+        switch(status.toUpperCase()){
+          case 'CANCELADA':
+          cl += ' btn-danger';
+          break;
+          case 'ATENDIDA':        
+          cl += ' btn-success';
+          break;
+          case 'EM ANDAMENTO':
+          cl += ' btn-secondary';
+          break;
+          case 'AGUARDANDO RETIRADA':
+          cl += ' btn-info';
+          break;
+          case 'PENDENTE':
+          cl += ' btn-primary';
+          break;
+        }
+        return cl += ' btn-sm';
+      }     
+      
+      public filterForms(evt: any): void {
+        if (this.termoBuscaChanged.observers.length === 0) {
+          this.termoBuscaChanged
+            .pipe(debounceTime(1000))
+            .subscribe((filtrarPor) => {
+              this.spinner.show();
+              this.formService
+                .getForms(
+                  this.pagination.currentPage,
+                  this.pagination.itemsPerPage,
+                  filtrarPor
+                )
+                .subscribe(
+                  (paginatedResult: PaginatedResult<Form[]>) => {
+                    this.forms = paginatedResult.result;
+                    this.pagination = paginatedResult.pagination;
+                  },
+                  (error: any) => {
+                    this.spinner.hide();
+                    this.toastr.error('Erro ao Carregar os Formulários', 'Erro!');
+                  }
+                )
+                .add(() => this.spinner.hide());
+            });
+        }
+        this.termoBuscaChanged.next(evt.value);
+      }
+        
+        openModal(template: TemplateRef<any>) {
+          this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+        }
+        
+        confirm(): void {
+          this.modalRef?.hide();
+          this.toastr.success("mensagem corpo", "cabeçalho");
+        }
+        
+        decline(): void {
+          this.modalRef?.hide();
+        }
+        
+        formDetail(formId: number): void{
+          this.router.navigate([`forms/detail/${formId}`])
+        }
+        
+        pageChanged(event: any): void{
+          this.pagination.currentPage = event.page;
+          this.getForms();
+        }
+        
+      }
+      
